b302ddec855748268ce6e87f9a4fd30d350bc2b7
Oded Winberger <odedindi@gmail.com>
2022-02-19 16:08:46 +0100

    add pwa

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const withPWA = require('next-pwa');
+
+module.exports = withPWA({
+	pwa: {
+		disable: process.env.NODE_ENV === 'development',
+		dest: 'public',
+	},
 	reactStrictMode: true,
 
 	images: {
 		domains: ['images.unsplash.com', 'https://icons8.com/', 'https://maxcdn.icons8.com']
 	}
-};
+});
